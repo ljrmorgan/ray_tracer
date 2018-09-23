8069b41d95196639981d8fd9c455a2a3f0711bdc
Louis Morgan <ljrmorgan@gmail.com>
2018-09-23 16:42:35 +0100

    Fix bug in vec3::cross

--- vec3.h
+++ vec3.h
@@ -96,7 +96,7 @@ inline float dot(const vec3 &v1, const vec3 &v2) {
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return vec3{
         v1[1] * v2[2] - v1[2] * v2[1],
-        v1[0] * v2[2] - v1[2] * v2[0],
+        -(v1[0] * v2[2] - v1[2] * v2[0]),
         v2[0] * v2[1] - v1[1] * v2[0]
     };
 }
